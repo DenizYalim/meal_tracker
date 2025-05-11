e191baa150bb7b3a607d8532997010b30224c71d
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-05-11 13:21:36 +0300

    buttons to recipelist

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -65,10 +65,22 @@ function RecipeList({}: {}) {
   
   return (
     <div style={{ border: "2px solid white", padding: "10px" }}>
-      <h2>Recipelist :</h2>
-      <ul>
-         {[...recipeListMap.keys()].map((key) => (
-          <li key={key}>{key}</li>
+      <h1>Food Data Keys</h1>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        {[...recipeListMap.keys()].map((key) => (
+          <li
+            key={key}
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              padding: '10px',
+              //borderBottom: '1px solid #ccc',
+            }}
+          >
+            <span>{key}</span>
+            <button style={{ cursor: 'pointer', padding: '5px 10px' }}>+</button> {/* Button should make api call with 100 grams  /addMealtoDay/<int:date> */}
+          </li>
         ))}
       </ul>
     </div>
