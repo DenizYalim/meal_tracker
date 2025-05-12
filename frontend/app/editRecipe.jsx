c8808a87435bc044f707970661ef7fab84543ef6
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-05-12 23:08:50 +0300

    sepereated react componenets to different files

---
+++ frontend/app/editRecipe.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+function EditRecipe({ }) { // endpoint : addrecipe
+    return (<div style={{ border: "2px solid white", padding: "10px" }}>Edit Recipe</div>)
+}
+
+
+export default EditRecipe;
